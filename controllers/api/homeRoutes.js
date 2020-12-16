b50614cf99ab929d11c1707a8d64454c71c775f7
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2020-12-15 18:25:34 -0700

    homeRoutes updated with .get API method to get all users sorted by name, translate data so Handlebars can read it with .map method and pass data into Handlebars template

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
 const { User } = require('../models');
 
-//Get users
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findall({
+            attributes: { exclude: ['password'] },
+            order: [['name', 'ASC']],
+        });
+
+        const users = userData.map((project) => project.get({ plain: true }));
+
+        res.render('homepage', { users });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
