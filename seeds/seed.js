08d9de09ba9428a174283cefdbcf6c3639012762
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2020-12-15 21:55:02 -0700

    Placeholders added to dashboard and homepage.  Blog seeding added to seed.js file.

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,19 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Blog } = require('../models');
+
 
 const userData = require('./userData.json');
+const blogData = require('./blogData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
     await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Blog.bulkCreate(blogData, {
         individualHooks: true,
         returning: true,
     });
