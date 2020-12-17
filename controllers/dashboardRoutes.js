84ced3394f5d38f29b2d337dde02809938f93d70
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2020-12-17 00:04:16 -0700

    dashboardRoutes controller created.  GET method created to render dashboard page.  yourPost property added to Post model and postData samples to filter out posts created by logged in user.

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { Post } = require('..models');
+
+router.get('/dashboard', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {
+                yourPost: true,
+            },
+        });
+
+        const yourPosts = postData.map((project) => project.get({ plain: true }));
+
+        res.render('dashboard', { yourPosts });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
