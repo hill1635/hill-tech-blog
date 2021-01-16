6423510529c058d11ede6156100e09efa84d3bf5
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-01-15 23:13:32 -0700

    Stylesheet linked to handlebars.  Add post button added to dashboard.  Add post function started.

--- public/js/post.js
+++ public/js/post.js
@@ -1,4 +1,17 @@
+const addPostBtn = document.querySelector(".addPost");
 //Function to add post
+const newPost = async () => {
+  const response = await fetch("/api/posts/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    //create elements here
+  } else {
+    alert(response.statusText);
+  }
+};
 //Query selector for input functions
 
 //Function to update post
