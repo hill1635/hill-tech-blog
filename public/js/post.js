2e2a1eb914f44f2fac716a97d791a5cbe7bdb060
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-01-16 00:07:18 -0700

    Rearranged some html elementsin dashboard handlebars.  Finished addPost function to generate a create new post div with inputs for title and content.  Linked to an addEventListener for the add post button.

--- public/js/post.js
+++ public/js/post.js
@@ -1,17 +1,32 @@
-const addPostBtn = document.querySelector(".addPost");
 //Function to add post
-const newPost = async () => {
-  const response = await fetch("/api/posts/", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-  });
+const addPost = async () => {
+  //   const response = await fetch("/api/posts/", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //   });
 
-  if (response.ok) {
-    //create elements here
-  } else {
-    alert(response.statusText);
-  }
+  //   if (response.ok) {
+  //create elements here
+  var body = document.querySelector(".body");
+  var newDiv = document.createElement("div");
+  var headerDiv = document.createElement("div");
+  var contentDiv = document.createElement("div");
+  var titleInput = document.createElement("input");
+  var contentInput = document.createElement("input");
+  var submitBtn = document.createElement("button");
+
+  body.appendChild(newDiv);
+  newDiv.appendChild(headerDiv);
+  newDiv.appendChild(contentDiv);
+  headerDiv.appendChild(titleInput);
+  contentDiv.appendChild(contentInput);
+  contentDiv.appendChild(submitBtn);
+  //   } else {
+  //     alert(response.statusText);
+  //   }
+  console.log("success");
 };
+
 //Query selector for input functions
 
 //Function to update post
@@ -19,3 +34,7 @@ const newPost = async () => {
 //Function to comment on post
 
 //Event listeners for buttons
+function init() {
+  document.querySelector(".addPost").addEventListener("click", addPost);
+}
+init();
