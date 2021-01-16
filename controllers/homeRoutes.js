7148c5e2e998c5edadf463faf3f951eaea34779a
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-01-15 22:39:09 -0700

    Added if statements to homepage and dashboard endpoints to load login page if user not logged into session.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,13 +3,18 @@ const { Post } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
+    console.log("req.session: ", req.session);
     const postData = await Post.findAll({
       order: [["datePosted", "DESC"]],
     });
 
     const posts = postData.map((project) => project.get({ plain: true }));
 
-    res.render("homepage", { posts });
+    if (req.session.logged_in === true) {
+      res.render("homepage", { posts });
+    } else {
+      res.render("login");
+    }
   } catch (err) {
     res.status(500).json(err);
   }
@@ -19,14 +24,18 @@ router.get("/dashboard", async (req, res) => {
   try {
     const postData = await Post.findAll({
       where: {
-        user_id: req.session.user_id
+        user_id: req.session.user_id,
       },
       order: [["datePosted", "DESC"]],
     });
 
     const myPosts = postData.map((project) => project.get({ plain: true }));
 
-    res.render("dashboard", { myPosts });
+    if (req.session.logged_in === true) {
+      res.render("dashboard", { myPosts });
+    } else {
+      res.render("login");
+    }
   } catch (err) {
     res.status(500).json(err);
   }
