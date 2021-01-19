65c47fec609cef838427f566999319f057c518a3
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-01-19 15:46:11 -0700

    Created elements for add post window with function to dynamically create window once Add Post button is clicked.  Added submitPost function to post the post to the database.  Event listener for button works, but POST method needs work.  Added GET route to postRoutes to be able to check if posts submit correctly.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,6 +2,16 @@
 const router = require("express").Router();
 const { Post } = require("../../models");
 
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({});
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //Create new post
 router.post("/", async (req, res) => {
   try {
--- public/js/post.js
+++ public/js/post.js
@@ -1,30 +1,40 @@
+var body = document.querySelector(".body");
+var newDiv = document.createElement("div");
+var headerDiv = document.createElement("div");
+var contentDiv = document.createElement("div");
+var titleInput = document.createElement("input");
+var contentInput = document.createElement("input");
+var submitBtn = document.createElement("button");
+
+const submitPost = async () => {
+  var title = titleInput.value;
+  var content = contentInput.value;
+
+  //Does not work
+  const response = await fetch("/api/posts/", {
+    method: "POST",
+    body: JSON.stringify({ title: title, content: content, user_id : this.user_id }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    headerDiv.setAttribute("style", "display: none");
+  } else {
+    console.log(response.statusText);
+  }
+};
+
 //Function to add post
 const addPost = async () => {
-  //   const response = await fetch("/api/posts/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/json" },
-  //   });
-
-  //   if (response.ok) {
   //create elements here
-  var body = document.querySelector(".body");
-  var newDiv = document.createElement("div");
-  var headerDiv = document.createElement("div");
-  var contentDiv = document.createElement("div");
-  var titleInput = document.createElement("input");
-  var contentInput = document.createElement("input");
-  var submitBtn = document.createElement("button");
-
   body.appendChild(newDiv);
   newDiv.appendChild(headerDiv);
   newDiv.appendChild(contentDiv);
   headerDiv.appendChild(titleInput);
   contentDiv.appendChild(contentInput);
   contentDiv.appendChild(submitBtn);
-  //   } else {
-  //     alert(response.statusText);
-  //   }
-  console.log("success");
+
+  submitBtn.addEventListener("click", submitPost);
 };
 
 //Query selector for input functions
