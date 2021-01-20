8baf3d3d6c5851696bf77532243872822d4ee0a0
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-01-19 19:19:00 -0700

    Added include to show on get route for userRoutes to tie posts to users.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Post } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
-    const userData = await User.findAll({});
+    const userData = await User.findAll({
+      include: { model: Post },
+    });
 
     res.status(200).json(userData);
   } catch (err) {
